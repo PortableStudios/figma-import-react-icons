5dfc56dcbefabf8f17ab00a277e67d2788c54b88
Darcy Glennen <darcy@portable.com.au>
2021-04-27 15:50:27 +1000

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,18 @@ const fs = require('fs');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const reactIconsPackageJson = path.resolve(
   __dirname,
   './node_modules/react-icons/package.json'
 );
+
+const getReactIconsVersion = () => {
+  const file = fs.readFileSync(reactIconsPackageJson);
+  const json = JSON.parse(file);
+  return json.version;
+};
 
 // Based on https://github.com/figma/plugin-samples/tree/master/webpack
 module.exports = (env, argv) => {
@@ -28,7 +33,7 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.scss$/,
-          loader: ['style-loader', 'css-loader', 'sass-loader'],
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
       ],
     },
@@ -46,19 +51,37 @@ module.exports = (env, argv) => {
         // At compile time read the version from the `react-icons` package.json
         // Then inject the value so we can display it in the UI
         REACT_ICONS_VERSION: webpack.DefinePlugin.runtimeValue(() => {
-          const file = fs.readFileSync(reactIconsPackageJson);
-          const json = JSON.parse(file);
-          return JSON.stringify(json.version);
+          return JSON.stringify(getReactIconsVersion());
         }, [reactIconsPackageJson]),
       }),
       // Generate "ui.html" and inline the "ui.ts" file
       new HtmlWebpackPlugin({
-        template: './src/ui.html',
         filename: 'ui.html',
         inlineSource: '.(js)$',
         chunks: ['ui'],
+        inject: false,
+        // Based on https://github.com/ph1p/figma-chat/blob/master/webpack.config.js
+        templateContent: ({ compilation, htmlWebpackPlugin }) => `
+          <html>
+            <body>
+              <div class="container js-container">
+                <span class="header">
+                  using <a href="https://react-icons.github.io/react-icons/" target="_blank">react-icons</a> v${getReactIconsVersion()}</span>
+                </span>
+                <div class="buttons js-buttons"></div>
+                <div class="loading-container js-loading-container">
+                  <div class="loading-indicator"></div>
+                </div>
+              </div>
+              ${htmlWebpackPlugin.files.js.map(
+                (jsFile) =>
+                  `<script>${compilation.assets[
+                    jsFile.substr(htmlWebpackPlugin.files.publicPath.length)
+                  ].source()}</script>`
+              )}
+            </body>
+          </html>`,
       }),
-      new HtmlWebpackInlineSourcePlugin(HtmlWebpackPlugin),
       // Add bundle analyzer plugin if ANALYZE environment variable is true
       ...(process.env.ANALYZE ? [new BundleAnalyzerPlugin()] : []),
     ],
